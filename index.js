79d58916ed7928a990efd999658dbd33c35ec4f0
alexeypetrushin <alexey.petrushin@gmail.com>
2013-04-22 01:28:40 +0400

    Adding `app.__render` hook.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var compile = function(file, data) {
   var eco = require('eco')
   var content = eco.compile(data)
-  return "module.exports = " + content + ";"
+  var result = []
+  result.push("var template = " + content + ";")
+
+  // Allow to add hook to render, if `window.app.__render` defined it will be called
+  // as `app.__render(template, variables)`.
+  // It's usefull if you want to alter how template works, for example inject additional
+  // variables.
+  result.push("module.exports = function(variables){")
+  result.push("  if(window.app.__render) return window.app.__render(template, variables);")
+  result.push("  else return template(variables);")
+  result.push("};")
+
+  return result.join("\n")
 }
 
 var isEco = function (file) {
